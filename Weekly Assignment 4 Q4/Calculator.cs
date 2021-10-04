35b80b2a306a123ef6697552fe2c1a084c4f1c8b
hxchua00 <hxchua00@gmail.com>
2021-10-04 15:48:49 +0800

    Add project files.

---
+++ Weekly Assignment 4 Q4/Calculator.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Weekly_Assignment_4_Q4
+{
+    class Calculator
+    {
+        public void Add(int a, int b)
+        {
+            try 
+            {
+                int result = a + b;
+                Console.WriteLine("Sum of the 2 numbers: " + result);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            catch (ArithmeticException e)
+            {
+                throw e;
+            }
+            catch (FormatException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        public void Subtract(int a, int b)
+        {   
+            try
+            {
+                int result = a - b;
+                Console.WriteLine("Difference bewtween the 2 numbers: " + result);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            catch (ArithmeticException e)
+            {
+                throw e;
+            }
+            catch (FormatException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        public void Multiply(int a, int b)
+        {
+            
+            try
+            {
+                int result = a * b;
+                Console.WriteLine("Multiplication of the 2 numbers: " + result);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            catch (ArithmeticException e)
+            {
+                throw e;
+            }
+            catch (FormatException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        public void Divide(double a, double b)
+        { 
+            try
+            {
+                double result = a / b;
+                Console.WriteLine("Division of the 2 numbers: " + result);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            catch (ArithmeticException e)
+            {
+                throw e;
+            }
+            catch (FormatException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        public void Remainder(double a, double b)
+        {
+            try
+            {
+                double result = a % b;
+                Console.WriteLine("Remainder of the 2 numbers: " + result);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            catch (ArithmeticException e)
+            {
+                throw e;
+            }
+            catch (FormatException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        public void Power(double a, double b)
+        {
+            try
+            {
+                double result = Math.Pow(a,b);
+                Console.WriteLine("Power of the 2 numbers: " + result);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            catch (ArithmeticException e)
+            {
+                throw e;
+            }
+            catch (FormatException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ Weekly Assignment 4 Q4/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Weekly_Assignment_4_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Thread main = new Thread(() => { UserMenu(); });
+                main.Priority = ThreadPriority.Highest;
+                main.Start();
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("The following error has occured: " + e.Message);
+                Console.WriteLine("StackTrace: '{0}'", Environment.StackTrace);
+                Console.ReadLine();
+            }   
+        }
+
+        public static void UserMenu()
+        {
+            Calculator cal = new Calculator();
+            bool cont = true;
+
+            try
+            {
+                Console.WriteLine("Enter 2 numbers: ");
+                Console.Write("First Number: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Second Number: ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                while (cont)
+                {
+                    Console.WriteLine("What operation would you like to do?");
+                    Console.WriteLine();
+                    Console.WriteLine("1) Addition");
+                    Console.WriteLine("2) Subtraction");
+                    Console.WriteLine("3) Multiply");
+                    Console.WriteLine("4) Divide");
+                    Console.WriteLine("5) Remainder");
+                    Console.WriteLine("6) Power");
+                    Console.WriteLine("7) Exit");
+                    Console.WriteLine();
+
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (choice)
+                    {
+                        case 1:
+                            Thread add = new Thread(() => { cal.Add(x, y); });
+                            add.Start();
+
+                            if (Thread.CurrentThread == add)
+                            {
+                                add.Abort();
+                            }
+                            break;
+                        case 2:
+                            Thread sub = new Thread(() => { cal.Subtract(x, y); });
+                            sub.Start();
+
+                            if (Thread.CurrentThread == sub)
+                            {
+                                sub.Abort();
+                            }
+                            break;
+                        case 3:
+                            Thread mul = new Thread(() => { cal.Multiply(x, y); });
+                            mul.Start();
+
+                            if (Thread.CurrentThread == mul)
+                            {
+                                mul.Abort();
+                            }
+                            break;
+                        case 4:
+                            Thread div = new Thread(() => { cal.Divide(x, y); });
+                            div.Start();
+
+                            if (Thread.CurrentThread == div)
+                            {
+                                div.Abort();
+                            }
+                            break;
+                        case 5:
+                            Thread rem = new Thread(() => { cal.Remainder(x, y); });
+                            rem.Start();
+
+                            if (Thread.CurrentThread == rem)
+                            {
+                                rem.Abort();
+                            }
+                            break;
+                        case 6:
+                            Thread pow = new Thread(() => { cal.Power(x, y); });
+                            pow.Start();
+
+                            if (Thread.CurrentThread == pow)
+                            {
+                                pow.Abort();
+                            }
+                            break;
+                        case 7:
+                            cont = false;
+                            Console.WriteLine("Goodbye!");
+                            Console.WriteLine();
+                            break;
+                        default:
+                            Console.WriteLine("Error! No such option!");
+                            Console.WriteLine();
+                            break;
+                    }
+                }
+            }
+            catch (MethodAccessException e)
+            {
+                throw e;
+            }
+            catch (MemberAccessException e)
+            {
+                throw e;
+            }
+            catch (InvalidCastException e)
+            {
+                throw e;
+            }
+            catch (DuplicateWaitObjectException e)
+            {
+                throw e;
+            }
+            catch (OverflowException e)
+            {
+                throw e;
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            catch (ArithmeticException e)
+            {
+                throw e;
+            }
+            catch (FormatException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+
+        }
+    }
+}
